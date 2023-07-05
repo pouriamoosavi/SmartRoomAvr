18c8c3df74c0ee512af8c6afd81efdfeda2cd924
Pouria <pouria.moosvi@gmail.com>
2023-07-05 20:38:06 +0330

    Change pwm to phase correct mode to fix ds3232 seconds bug 2

--- src/main.c
+++ src/main.c
@@ -56,9 +56,8 @@ void println(char* str) {
 }
 
 void PWM_init() {
-	/*set fast PWM mode with non-inverted output*/
 	TCCR0 = (1<<WGM00) | (1<<COM01) | (1<<CS00);
-	DDRB|=(1<<PB3);  /*set OC0 pin as output*/
+	DDRB |= (1<<PB3);  /*set OC0 pin as output*/
 }
 
 void printOnLcd(LiquidCrystalDevice_t lcd1) {
